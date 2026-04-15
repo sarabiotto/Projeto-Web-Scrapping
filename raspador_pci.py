419eda35464716d59555aec15bc07d24dd444d1b
Sara Biotto <sarabiotto@gmail.com>
2026-04-15 10:48:53 -0300

    adicinado site e requests

--- raspador_pci.py
+++ raspador_pci.py
@@ -1,2 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+
+url = 'https://www.pciconcursos.com.br/concursos/sudeste/'
+print("Acessando o site PCI Concursos...")
+
+res = requests.get(url)
+res.raise_for_status()
